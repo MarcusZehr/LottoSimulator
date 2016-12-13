9df69e5ef76a03c10fda5470526b9f995b407b3f
zehrm <mzehr@innovasi.com>
2016-12-13 16:41:47 -0500

    organized lottoService tests by function and added empty tests

--- Specs/Services/lottoServiceTests.js
+++ Specs/Services/lottoServiceTests.js
@@ -18,14 +18,68 @@
 
     }));
 
-    it('generatePowerBall() to create a number between 1 and 26', function () {
-        //Arrange
-        //Act
-        var powerBall = lottoService.generatePowerBall();
+    describe('generateUniqueWhiteBalls()', function () {
 
-        //Assert
-        expect(powerBall).not.toBeUndefined();
-        expect(powerBall).toBeGreaterThan(0);
-        expect(powerBall).toBeLessThan(27);
+        it('should create an array with a length of 5', function () {
+            //Arrange
+            //Act
+            var whiteBalls = lottoService.generateUniqueWhiteBalls();
+
+            //Assert
+            expect(whiteBalls.length).toEqual(5);
+        });
+
+        it('should create an array consisting of unique numbers', function () {
+            //Arrange
+            //Act
+            //Assert
+        });
+
+        it('should have its elements sorted in ascending order', function () {
+            //Arrange
+            //Act
+            //Assert
+        });
+    });
+
+    describe('generatePowerBall()', function () {
+
+        it('should generate a number between 1 and 26', function () {
+            //Arrange
+            //Act
+            var powerBall = lottoService.generatePowerBall();
+
+            //Assert
+            expect(powerBall).toBeGreaterThan(0);
+            expect(powerBall).toBeLessThan(27);
+        });
+    });
+
+    describe('addNewPick()', function () {
+
+        it('', function() {
+            
+        });
+    });
+
+    describe('removePick()', function () {
+
+        it('', function () {
+
+        });
+    });
+
+    describe('checkForWinningNumbers()', function () {
+
+        it('', function () {
+
+        });
+    });
+
+    describe('_shuffle', function () {
+
+        it('', function () {
+
+        });
     });
 });
