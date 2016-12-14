d3b870f70f0976622ade644aa3032a6e70e4c272
zehrm <mzehr@innovasi.com>
2016-12-14 12:07:11 -0500

    Added tests for all current functionality of the lottoService

--- Specs/Services/lottoServiceTests.js
+++ Specs/Services/lottoServiceTests.js
@@ -31,14 +31,39 @@
 
         it('should create an array consisting of unique numbers', function () {
             //Arrange
+            function hasDuplicates(array) {
+                var valuesSoFar = [];
+                for (var i = 0; i < array.length; ++i) {
+                    var value = array[i];
+                    if (valuesSoFar.indexOf(value) !== -1) {
+                        return true;
+                    }
+                    valuesSoFar.push(value);
+                }
+                return false;
+            }
             //Act
+            var whiteBalls = lottoService.generateUniqueWhiteBalls();
+
             //Assert
+            expect(hasDuplicates(whiteBalls)).toBeFalsy();
         });
 
         it('should have its elements sorted in ascending order', function () {
             //Arrange
+            function isSorted(array) {
+                for (var i = 0; i < array.length-1; i++) {
+                    if (array[i] > array[i + 1])
+                        return false;
+                }
+                return true;
+            }
+
             //Act
+            var whiteBalls = lottoService.generateUniqueWhiteBalls();
+
             //Assert
+            expect(isSorted(whiteBalls)).toBeTruthy();
         });
     });
 
@@ -57,29 +82,63 @@
 
     describe('addNewPick()', function () {
 
-        it('', function() {
-            
+        it('should add an element to an array', function() {
+            //Arrange
+            var picks = [];
+
+            //Act
+            lottoService.addNewPick(picks);
+
+            //Assert
+            expect(picks.length).toEqual(1);
+        });
+
+        it('should add a pick with a valid index and all ball numbers being null', function () {
+            //Arrange
+            var picks = [];
+
+            //Act
+            lottoService.addNewPick(picks);
+
+            //Assert
+            expect(picks[0].index).not.toBeNull();
+
+            expect(picks[0].w1).toBeNull();
+            expect(picks[0].w2).toBeNull();
+            expect(picks[0].w3).toBeNull();
+            expect(picks[0].w4).toBeNull();
+            expect(picks[0].w5).toBeNull();
+            expect(picks[0].pb).toBeNull();
         });
     });
 
     describe('removePick()', function () {
 
-        it('', function () {
+        it('should remove the last element from an array', function () {
+            //Arrange
+            var picks = [
+                { index: 0, w1: 1, w2: 2, w3: 3, w4: 4, w5: 5, pb: 6 },
+                { index: 1, w1: 7, w2: 8, w3: 9, w4: 1, w5: 2, pb: 3 }
+            ];
 
+            //Act
+            lottoService.removePick(picks);
+
+            //Assert
+            expect(picks.length).toEqual(1);
+            expect(picks[1]).toBeUndefined();
+
+            expect(picks[0].w1).toEqual(1);
         });
     });
 
     describe('checkForWinningNumbers()', function () {
 
         it('', function () {
-
+            //Arrange
+            //Act
+            //Assert
         });
     });
 
-    describe('_shuffle', function () {
-
-        it('', function () {
-
-        });
-    });
 });
